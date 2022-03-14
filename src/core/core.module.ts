e5eb684f06be518e0e9f0b2a18d9cc3556574b27
EightyMountain <eightymountain@gmail.com>
2022-03-14 11:49:27 +0900

    feat: http exception filter

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,12 +1,14 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpRequestInterceptor } from './interceptors/http.request.interceptor';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
 import { LoggerMiddleware } from './middleware/logger.middleware';
+import { HttpExceptionFilter } from '@core/filters/http.exception.filter';
 
 @Module({
   providers: [
     { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
     // { provide: APP_INTERCEPTOR, useClass: HttpRequestInterceptor },
   ],
 })
