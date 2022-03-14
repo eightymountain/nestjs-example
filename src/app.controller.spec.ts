f855ead24b26a8ebc1dbb7a3bab6db92eef2a2ba
EightyMountain <eightymountain@gmail.com>
2022-03-14 10:17:43 +0900

    test: health check

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('heath check', () => {
+      expect(appController.heathCheck()).toBe('let`s do that shit!');
     });
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('health')
-  health(): string {
+  heathCheck(): string {
     return this.appService.health();
   }
 
